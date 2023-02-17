66a4c01c873c22b060ef4d225e5dfa49238ed3d1
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-02-17 09:28:13 +0200

    Added player generation algorithm

---
+++ Services/Services/PlayerStatsGen/ValueRandomGen.cs
@@ -0,0 +1,37 @@
+﻿namespace FootBalLife.Services.PlayerStatsGen
+{
+    public class ValueRandomGen
+    {
+        // отталкиваемся
+        private readonly long _mean;
+        // разброс
+        private readonly long _stdDev;
+
+        private readonly long _minVal;
+        private readonly long _maxVal;
+
+        public ValueRandomGen(long mean, long stdDev, long minVal = 1, long maxVal = 100)
+        {
+            _mean = mean;
+            _stdDev = stdDev;
+            _minVal = minVal;
+            _maxVal = maxVal;
+        }
+
+        public double Next()
+        {
+            return Next(_mean, _stdDev, _minVal, _maxVal);
+        }
+        public static long Next(long mean, long stdDev, long minVal = 1, long maxVal = 100)
+        {
+            Random random = new();
+
+            long u1 = random.NextInt64();
+            long u2 = random.NextInt64();
+
+            long randStdNormal = (long)(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2));
+
+            return Math.Max(Math.Min(mean + stdDev * randStdNormal, maxVal), minVal);
+        }
+    }
+}
