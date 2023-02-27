88e8c895d3adf049f4518249fb19df7c5b09b397
Serhiy <sergey_02_10@ukr.net>
2023-02-27 21:07:37 +0200

    Fixed DatabaseLayer

--- DatabaseLayer/Model/Team.cs
+++ DatabaseLayer/Model/Team.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using FootBalLife.Database.Repositories;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FootBalLife.Database
@@ -17,6 +18,21 @@ namespace FootBalLife.Database
         public string ScoutID { get; set; }
         public string CoachID { get; set; }
         public string SportsDirectorId { get; set; }
+
+
+        private List<Player> _players;
+        public List<Player> Players { get 
+            {  
+                if(_players == null)
+                {
+                    _players = new List<Player>();
+                    var playerRepos = new PlayerRepository();
+                    var players = playerRepos.Retrive(this.Id);
+                    _players.AddRange(players);
+                }
+                return _players; 
+            } 
+        }
         /*
         public virtual ICollection<Contract> Contracts { get; internal set; } = new List<Contract>();
 
@@ -25,21 +41,21 @@ namespace FootBalLife.Database
         public ICollection<Match> MatchGuestTeamNavigations { get; internal set; } = new List<Match>();
         
         public ICollection<NationalResultTable> NationalResultTables { get; internal set; } = new List<NationalResultTable>();
-        
+        */
         // returns first player that have playerPostion
         public Player GetPlayer(PlayerPosition playerPostion)
         {
             //Contracts.First().Players
 
-            var selectedPlayers = Contracts.Where(player => player.PositionID == (long)playerPostion);
-            return selectedPlayers.MaxBy(player => player.Endurance);
+            var selectedPlayers = Players.Where(player => player.Position.Location == playerPostion);
+            return selectedPlayers.FirstOrDefault();
         }
         public double AvgSpeed(PlayerPosition playerPostion = PlayerPosition.All)
         {
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Speed)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Speed);
         }
         public double AvgStrike(PlayerPosition playerPostion = PlayerPosition.All)
@@ -47,7 +63,7 @@ namespace FootBalLife.Database
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Strike)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Strike);
         }
         public double AvgDefense(PlayerPosition playerPostion = PlayerPosition.All)
@@ -55,7 +71,7 @@ namespace FootBalLife.Database
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Endurance)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Endurance);
         }
         public double AvgPhysicalTraining(PlayerPosition playerPostion = PlayerPosition.All)
@@ -63,7 +79,7 @@ namespace FootBalLife.Database
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Physics)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Physics);
         }
         public double AvgTechnique(PlayerPosition playerPostion = PlayerPosition.All)
@@ -71,7 +87,7 @@ namespace FootBalLife.Database
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Technique)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Technique);
         }
         public double AvgPassing(PlayerPosition playerPostion = PlayerPosition.All)
@@ -79,8 +95,8 @@ namespace FootBalLife.Database
             return playerPostion == PlayerPosition.All
                 ? Players.Average(p => p.Passing)
                 : Players
-                    .Where(p => p.PositionID == (long)playerPostion)
+                    .Where(p => p.Position.Location == playerPostion)
                     .Average(p => p.Passing);
-        }*/
+        }
     }
 }
