2155976c61efaa3a75f3d1cc0ba4fed77a9efa4d
Serhiy <sergey_02_10@ukr.net>
2023-03-04 13:44:13 +0200

    Add GoalRepository

--- DatabaseLayer/Model/Goal.cs
+++ DatabaseLayer/Model/Goal.cs
@@ -2,10 +2,11 @@
 {
     public class Goal
     {
-        public string Id { get; set; }
+        public string Id { get; internal set; }
         public string MatchId { get; set; }
         public string PlayerId { get; set; }
         public string TeamId { get; set; }
         public int MatchMinute { get; set; }
+        public string AssistPlayerId { get; set; }
     }
 }
