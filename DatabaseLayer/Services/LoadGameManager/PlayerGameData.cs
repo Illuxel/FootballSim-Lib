cb7bdd5ef9102989ce8e4ffa6001d1f393863bdd
9Sanya <bagira22072015@gmail.com>
2023-04-21 20:29:25 +0300

    Update PlayerGameData.cs

--- DatabaseLayer/Services/LoadGameManager/PlayerGameData.cs
+++ DatabaseLayer/Services/LoadGameManager/PlayerGameData.cs
@@ -6,15 +6,18 @@ namespace DatabaseLayer.Services
     {
         public string Id { get; private set; }
         public string PlayerName { get; set; }
-
+        public string PlayerSurname { get; set; }
+        public string Club { get; set; }
         public string Date { get; set; }
 
         public double Money { get; set; }
 
-        public PlayerGameData(string playerName, string date, double money)
+        public PlayerGameData(string playerName,string playerSurname, string club, string date, double money)
         {
             this.Id = Guid.NewGuid().ToString();
             this.PlayerName = playerName;
+            this.PlayerSurname = playerSurname;
+            this.Club = club;
             this.Money = money;
             this.Date = date;
         }
