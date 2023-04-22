9eb851e2f99b8395799a8682ad3e3a8536fa8c20
Drakanil <122986739+Drakanil@users.noreply.github.com>
2023-04-22 13:51:19 +0300

    Update FourFourTwoSchemeDefenition.cs

--- BusinessLogicLayer/Services/PlayerPositionDeterminer/TacticSchemeDefinition/FourFourTwoSchemeDefenition.cs
+++ BusinessLogicLayer/Services/PlayerPositionDeterminer/TacticSchemeDefinition/FourFourTwoSchemeDefenition.cs
@@ -8,24 +8,24 @@ namespace BusinessLogicLayer.Services
     {
         //index by positions:
         //-----1-----
-        //--2-3-4-5--
-        //--6-7-8-9--
-        //--10---11--
+        //--5-4-3-2--
+        //-16-15-13-12-
+        //--25---26--
         public Dictionary<int, string> GetPositions()
         {
             return new Dictionary<int, string>()
             {
                 { 1, EnumDescription.GetEnumDescription(PlayerPosition.Goalkeeper) },
-                { 2, EnumDescription.GetEnumDescription(PlayerPosition.RightDefender) },
-                { 3, EnumDescription.GetEnumDescription(PlayerPosition.CentralDefender) },
+                { 5, EnumDescription.GetEnumDescription(PlayerPosition.RightDefender) },
                 { 4, EnumDescription.GetEnumDescription(PlayerPosition.CentralDefender) },
-                { 5, EnumDescription.GetEnumDescription(PlayerPosition.LeftDefender) },
-                { 6, EnumDescription.GetEnumDescription(PlayerPosition.RightMidfielder) },
-                { 7, EnumDescription.GetEnumDescription(PlayerPosition.CentralMidfielder) },
-                { 8, EnumDescription.GetEnumDescription(PlayerPosition.CentralMidfielder) },
-                { 9, EnumDescription.GetEnumDescription(PlayerPosition.LeftMidfielder) },
-                { 10, EnumDescription.GetEnumDescription(PlayerPosition.Forward) },
-                { 11, EnumDescription.GetEnumDescription(PlayerPosition.Forward) }
+                { 3, EnumDescription.GetEnumDescription(PlayerPosition.CentralDefender) },
+                { 2, EnumDescription.GetEnumDescription(PlayerPosition.LeftDefender) },
+                { 16, EnumDescription.GetEnumDescription(PlayerPosition.RightMidfielder) },
+                { 15, EnumDescription.GetEnumDescription(PlayerPosition.CentralMidfielder) },
+                { 13, EnumDescription.GetEnumDescription(PlayerPosition.CentralMidfielder) },
+                { 12, EnumDescription.GetEnumDescription(PlayerPosition.LeftMidfielder) },
+                { 25, EnumDescription.GetEnumDescription(PlayerPosition.Forward) },
+                { 26, EnumDescription.GetEnumDescription(PlayerPosition.Forward) }
             };
         }
        
