00f2ea142445ad39b443adf9a4de0ba6380d6515
9Sanya <bagira22072015@gmail.com>
2023-04-27 00:52:56 +0300

    added Enum UserRole and modified PlayerGameData

---
+++ DatabaseLayer/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLayer.Enums
+{
+    public enum UserRole
+    {
+        Scout, 
+        Manager, 
+        Director
+    }
+}
