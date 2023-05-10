5e204f311b20396fc5a22bf94502ddba2d34ef07
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2023-05-10 10:04:24 +0300

    Added ~ Update(NationalResultTable) ~ Retrieve(teamId, season) ~ Insert(NationalResultTable) ~ Delete (teamId, season)

--- DatabaseLayer/Repositories/NationalResTabRepository.cs
+++ DatabaseLayer/Repositories/NationalResTabRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using DatabaseLayer.DBSettings;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SQLite;
 using System.Net.NetworkInformation;
 
@@ -28,6 +29,97 @@ namespace DatabaseLayer.Repositories
                     splitOn: "ID").AsList();
             }
             return result;
+        }
+
+        public bool Update(NationalResultTable model)
+        {
+            bool result = false;
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                var record =
+                    connection.QuerySingleOrDefault(
+                        @$"SELECT * FROM NationalResultTable WHERE TeamId = @TeamId AND Season = @Season", model);
+
+                if (record == null)
+                    return result;
+
+                var rowAffected = connection.Execute(
+                     "UPDATE NationalResultTable SET "+
+                        "Wins = @wins, Draws = @draws,"+
+                        "Loses = @loses, ScoredGoals = @scoredGoals, MissedGoals = @missedGoals,"+
+                        "TotalPosition = @totalPosition WHERE TeamId = @teamId AND Season = @season", 
+                    param: new { wins = model.Wins, 
+                        draws = model.Draws, 
+                        loses = model.Loses, 
+                        scoredGoals = model.ScoredGoals, 
+                        missedGoals = model.MissedGoals, 
+                        totalPosition = model.TotalPosition,
+                        teamId = model.TeamID,
+                        season = model.Season
+                    });
+                result = rowAffected == 1;
+            }
+
+            return result;
+        }
+
+        public List<NationalResultTable> Retrieve(string teamId, string season)
+        {
+            var result = new List<NationalResultTable>();
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                result = connection.Query<NationalResultTable, Team, NationalResultTable>(
+                    "SELECT e.*, t.* " +
+                    "from NationalResultTable e " +
+                    "inner join Team t on e.TeamId = t.ID " +
+                    "WHERE t.ID = @teamId AND e.Season = @season;",
+                    (nationalTable, team) =>
+                    {
+                        nationalTable.Team = team;
+                        return nationalTable;
+                    },
+                    param: new { teamId = teamId, season = season },
+                    splitOn: "ID").AsList();
+            }
+            return result;
+        }
+
+        public bool Insert(NationalResultTable model)
+        {
+            bool result = false;
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+
+                var record = connection.QueryFirstOrDefault(
+                    "SELECT * FROM NationalResultTable WHERE TeamId = @teamId AND Season = @season",
+                    param: new { teamId = model.TeamID, season = model.Season }
+                );
+
+                if (record != null)
+                    return result;
+
+                var rowAffected = connection.Execute(
+                    @"INSERT INTO NationalResultTable (Season, TeamId, Wins, Draws, Loses, ScoredGoals, MissedGoals, TotalPosition)
+                    VALUES (@Season, @TeamId, @Wins, @Draws, @Loses, @ScoredGoals, @MissedGoals, @TotalPosition)", model
+                    );
+                result = rowAffected == 0;
+            }
+            return result;
+        }
+
+        public bool Delete(string teamId, string season)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                var rowAffected = connection.Execute("DELETE FROM NationalResultTable WHERE TeamID = @TeamId AND Season = @Season",
+                    param: new { TeamId = teamId, Season = season });
+
+                return rowAffected == 1;
+            }
         }
     }
 }
