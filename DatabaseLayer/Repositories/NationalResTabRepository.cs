1bf405ba8c440748be70e555711a4d7e92ad1500
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2023-05-12 22:31:20 +0300

    Updated

--- DatabaseLayer/Repositories/NationalResTabRepository.cs
+++ DatabaseLayer/Repositories/NationalResTabRepository.cs
@@ -24,9 +24,10 @@ namespace DatabaseLayer.Repositories
                         nationalTable.Team = team;
                         return nationalTable;
                     },
-                    param: new { leagueId = leagueId, season = season },
+                    param: new { leagueId, season },
                     splitOn: "ID").AsList();
             }
+
             return result;
         }
 
@@ -36,27 +37,13 @@ namespace DatabaseLayer.Repositories
             using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
             {
                 connection.Open();
-                var record =
-                    connection.QuerySingleOrDefault(
-                        @$"SELECT * FROM NationalResultTable WHERE TeamId = @TeamId AND Season = @Season", model);
-
-                if (record == null)
-                    return result;
 
                 var rowAffected = connection.Execute(
                      "UPDATE NationalResultTable SET "+
-                        "Wins = @wins, Draws = @draws,"+
-                        "Loses = @loses, ScoredGoals = @scoredGoals, MissedGoals = @missedGoals,"+
-                        "TotalPosition = @totalPosition WHERE TeamId = @teamId AND Season = @season", 
-                    param: new { wins = model.Wins, 
-                        draws = model.Draws, 
-                        loses = model.Loses, 
-                        scoredGoals = model.ScoredGoals, 
-                        missedGoals = model.MissedGoals, 
-                        totalPosition = model.TotalPosition,
-                        teamId = model.TeamID,
-                        season = model.Season
-                    });
+                     "Wins = @wins, Draws = @draws,"+
+                     "Loses = @loses, ScoredGoals = @scoredGoals, MissedGoals = @missedGoals,"+
+                     "TotalPosition = @totalPosition WHERE TeamId = @teamId AND Season = @season", 
+                     param: new { model });
                 result = rowAffected == 1;
             }
 
@@ -79,9 +66,10 @@ namespace DatabaseLayer.Repositories
                         nationalTable.Team = team;
                         return nationalTable;
                     },
-                    param: new { teamId = teamId, season = season },
+                    param: new { teamId, season },
                     splitOn: "ID").AsList();
             }
+
             return result;
         }
 
@@ -92,20 +80,14 @@ namespace DatabaseLayer.Repositories
             {
                 connection.Open();
 
-                var record = connection.QueryFirstOrDefault(
-                    "SELECT * FROM NationalResultTable WHERE TeamId = @teamId AND Season = @season",
-                    param: new { teamId = model.TeamID, season = model.Season }
-                );
-
-                if (record != null)
-                    return result;
-
                 var rowAffected = connection.Execute(
                     @"INSERT INTO NationalResultTable (Season, TeamId, Wins, Draws, Loses, ScoredGoals, MissedGoals, TotalPosition)
                     VALUES (@Season, @TeamId, @Wins, @Draws, @Loses, @ScoredGoals, @MissedGoals, @TotalPosition)", model
                     );
+
                 result = rowAffected == 0;
             }
+
             return result;
         }
 
