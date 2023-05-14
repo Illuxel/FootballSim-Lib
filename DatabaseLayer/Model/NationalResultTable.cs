fd9bafb6164cbefe1a72fc26ad00d866dde3b0c0
Daniil <91062667+trnvdn@users.noreply.github.com>
2023-05-14 16:06:54 +0300

    Update NationalResultTable.cs

--- DatabaseLayer/Model/NationalResultTable.cs
+++ DatabaseLayer/Model/NationalResultTable.cs
@@ -2,18 +2,18 @@
 {
     public class NationalResultTable
     {
-        public string Season { get; internal set; }
+        public string Season { get; set; }
 
         public string TeamID { get; set; }
         public virtual Team Team { get; internal set; }
 
-        public long Wins { get; set; }
-        public long Draws { get; set; }
-        public long Loses { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Loses { get; set; }
 
-        public long ScoredGoals { get; set; }
-        public long MissedGoals { get; set; }
+        public int ScoredGoals { get; set; }
+        public int MissedGoals { get; set; }
 
-        public long TotalPosition { get; set; }
+        public int TotalPosition { get; set; }
     }
 }
