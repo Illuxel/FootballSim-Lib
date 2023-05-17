4604b2271eb0f8b8a816b29d6f514c343c3067fb
Daniil <ternak04@gmail.com>
2023-05-17 12:43:57 +0300

    Implemented model & repository

---
+++ DatabaseLayer/Model/Sponsor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLayer
+{
+    internal class Sponsor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DatabaseLayer/Repositories/SponsorRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using DatabaseLayer.DBSettings;
+using DatabaseLayer.Enums;
+using DatabaseLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DatabaseLayer.Repositories
+{
+    internal class SponsorRepository
+    {
+        public List<Sponsor> Retrieve()
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                return connection.Query<Sponsor>("SELECT * FROM Sponsor").AsList();
+            }
+        }
+        public Sponsor Retrieve(int ID)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                return connection.QueryFirstOrDefault<Sponsor>(
+                    @"SELECT * FROM Sponsor
+                      WHERE ID = @ID",new { ID });
+            }
+        }
+        public bool Insert(string Name)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                var record = connection.QuerySingleOrDefault<Team>("SELECT * FROM Sponsor WHERE Name = @Name", new { Name });
+                bool result = false;
+                if (record == null)
+                {
+                    var rowsAffected = connection.Execute(
+                        @"INSERT INTO Sponsor (ID, Name) 
+                          VALUES (@Name)", new { Name});
+                    result = rowsAffected == 1;
+                }
+                return result;
+            }
+        }
+        public bool Delete(int ID)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                var rowsAffected = connection.Execute("DELETE FROM Sponsor WHERE ID = @ID",
+                    new { ID });
+                return rowsAffected == 1;
+            }
+        }
+    }
+}
