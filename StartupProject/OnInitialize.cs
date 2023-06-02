07ebc73154f3dd84ba691d34e1c606ab237cb492
Serhiy <sergey_02_10@ukr.net>
2023-06-02 11:51:07 +0300

    Add download DB by http from VM

---
+++ StartupProject/OnInitialize.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogicLayer.Services;
+using System.Threading.Tasks;
+
+namespace StartupProject
+{
+    internal class OnInitialize
+    {
+        public static async Task Main(string[] args)
+        {
+            var dbDownloader = new DatabaseDownloader();
+            await dbDownloader.Download();
+            Program.Main(args);
+        }
+    }
+}
