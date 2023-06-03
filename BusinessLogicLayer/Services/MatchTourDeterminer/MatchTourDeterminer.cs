82e95b21c68a8ad682a49d565c5e33ffa3f78eef
Daniil <ternak04@gmail.com>
2023-06-03 18:39:25 +0300

    Updated to DB querry

--- BusinessLogicLayer/Services/MatchTourDeterminer/MatchTourDeterminer.cs
+++ BusinessLogicLayer/Services/MatchTourDeterminer/MatchTourDeterminer.cs
@@ -8,76 +8,15 @@ namespace BusinessLogicLayer.Services
 {
     public class MatchTourDeterminer
     {
-        int _leagueId;
-        DateTime _gameDate;
         MatchRepository _matchRepository;
 
-        public MatchTourDeterminer(int leagueId, DateTime gameDate)
+        public MatchTourDeterminer()
         {
-            _leagueId = leagueId;
-            _gameDate = gameDate;
             _matchRepository = new MatchRepository();
         }
-
-        private List<Match> GetAllMatchesByLeague(int leagueId)
+        public int DetermineTourNumber(int leagueId, DateTime gameDate)
         {
-            return _matchRepository.Retrieve(leagueId);
-        }
-
-        int GetLastTourNumber(List<Match> matches)
-        {
-            return matches[^1].TourNumber; 
-        }
-
-        private List<DateTime> GetSortedDates(List<Match> matches)
-        {
-            var dates = new HashSet<DateTime>();
-
-            foreach (var match in matches)
-            {
-                if (DateTime.TryParse(match.MatchDate, out DateTime date))
-                {
-                    dates.Add(date.Date);
-                }
-            }
-
-            var sortedDates = dates.OrderBy(x => x).ToList();
-
-            return sortedDates;
-        }
-
-        private DateTime GetNextDate(DateTime date, List<DateTime> dates)
-        {
-            foreach (var dateTime in dates)
-            {
-                if (dateTime > date)
-                {
-                    return dateTime;
-                }
-            }
-
-            return default;
-        }
-
-        private int GetCurrentTour(List<Match> matches, DateTime matchDate)
-        {
-            foreach (var match in matches)
-            {
-                if (DateTime.TryParse(match.MatchDate, out DateTime date) && date.Date == matchDate.Date)
-                {
-                    return match.TourNumber;
-                }
-            }
-            return -1;
-        }
-
-        public int DetermineTourNumber()
-        {
-            var matches = GetAllMatchesByLeague(_leagueId);
-            var lastNumber = GetLastTourNumber(matches);
-            var sortedDates = GetSortedDates(matches);
-            var closestDate = GetNextDate(_gameDate.Date, sortedDates);
-            var tourNumber = GetCurrentTour(matches, closestDate);
+            var tourNumber = _matchRepository.Retrieve(gameDate,leagueId);
             return tourNumber;
         }
     }
--- DatabaseLayer/Repositories/MatchRepository.cs
+++ DatabaseLayer/Repositories/MatchRepository.cs
@@ -189,6 +189,23 @@ namespace DatabaseLayer.Repositories
                 return rowsAffected != 1;
             }
         }
+        public int Retrieve(DateTime gameDate, int leagueId)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                {
+                    var response = connection.Query<int>(
+                        "SELECT TourNumber " +
+                        "FROM Match " +
+                        "WHERE date(MatchDate) >= date(@gameDate) AND LeagueId = @leagueId " +
+                        "ORDER BY date(MatchDate) " +
+                        "LIMIT 1", new { gameDate, leagueId }).First();
+                    return response;
+                }
+            }
+        }
+
     }
 }
 
