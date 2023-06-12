ced60625f310ac1471b65b9604ce2fecefc29e43
Serhiy <sergey_02_10@ukr.net>
2023-06-12 22:26:13 +0300

    Update TacticSchema.cs

--- DatabaseLayer/Enums/TacticSchema.cs
+++ DatabaseLayer/Enums/TacticSchema.cs
@@ -1,12 +1,20 @@
-﻿namespace DatabaseLayer
+﻿using System.ComponentModel;
+
+namespace DatabaseLayer
 {
     public enum TacticSchema
     {
+        [Description("4-3-3")]
         FourThreeThree,
+        [Description("4-4-2")]
         FourFourTwo,
+        [Description("4-5-1")]
         FourFiveOne,
+        [Description("3-5-2")]
         ThreeFiveTwo,
+        [Description("4-1-4-1")]
         FourOneFourOne,
+        [Description("4-4-1-1")]
         FourFourOneOne
     }
 }
