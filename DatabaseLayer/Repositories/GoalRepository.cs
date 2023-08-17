998eb659121e15c541057cb86355dc3dccfc5210
Daniil <ternak04@gmail.com>
2023-08-17 15:29:09 +0300

    Player stats model was implemented

--- DatabaseLayer/Repositories/GoalRepository.cs
+++ DatabaseLayer/Repositories/GoalRepository.cs
@@ -20,6 +20,52 @@ namespace DatabaseLayer.Repositories
             }
         }
 
+        public List<PlayerStatistic> GetTopGoalScorers(string leagueId, string season, string seasonStartDate, string seasonEndDate, int limit = 10)
+        {
+            using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
+            {
+                connection.Open();
+                var result = connection.Query(@"
+            SELECT
+                PIM.PlayerId,
+                SUM(CASE WHEN G.PlayerId = PIM.PlayerId THEN 1 ELSE 0 END) AS CountOfGoals,
+                COUNT(DISTINCT PIM.MatchId) AS CountOfPlayedMatches
+            FROM
+                PlayerInMatch PIM
+            JOIN
+                ""Match"" M ON PIM.MatchId = M.Id
+            LEFT JOIN
+                Goal G ON PIM.MatchId = G.MatchId AND PIM.PlayerId = G.PlayerId
+            WHERE
+                M.LeagueId = @leagueId AND Date(M.MatchDate) >= Date(@seasonStartDate) AND Date(M.MatchDate) < Date(@seasonEndDate)
+            GROUP BY
+                PIM.PlayerId
+            ORDER BY
+                CountOfGoals DESC,
+                CountOfPlayedMatches ASC
+            LIMIT @limit", new { leagueId, seasonStartDate, seasonEndDate, limit });
+
+                var playerRepository = new PlayerRepository(); // Create only one instance
+                var players = new List<PlayerStatistic>();
+
+                foreach (var player in result)
+                {
+
+                    var playerStatistic = new PlayerStatistic 
+                    {
+                        Player = playerRepository.RetrieveOne(player.PlayerId),
+                        CountOfGoals = (int)player.CountOfGoals,
+                        CountOfPlayedMatches = (int)player.CountOfPlayedMatches,
+                        Season = season
+                    };
+
+                    players.Add(playerStatistic);
+                }
+
+                return players;
+            }
+        }
+
         public bool Insert(Goal goal)
         {
             using (var connection = new SQLiteConnection(DatabaseManager.ConnectionString))
@@ -50,8 +96,8 @@ namespace DatabaseLayer.Repositories
                             goals);
                         var result = rowsAffected == 1;
 
+                        transaction.Commit();
                         return result;
-
                     }
                     catch (Exception ex)
                     {
--- StartupProject/Program.cs
+++ StartupProject/Program.cs
@@ -1,4 +1,5 @@
 using BusinessLogicLayer.Scenario;
+using BusinessLogicLayer.Services;
 using DatabaseLayer;
 using DatabaseLayer.Repositories;
 using System;
@@ -10,7 +11,16 @@ namespace StartupProject
     {
         public static void Main(string[] args)
         {
-
+            /*var tourStart = new DateTime(2023, 08, 12);
+            for (int i = 0; i < 38; i++)
+            {
+                var tourGen = new GenerateGameActionsToNextMatch(tourStart);
+                tourGen.SimulateActions();
+                tourStart = tourStart.AddDays(7);
+            }*/
+            GoalRepository goalRep = new GoalRepository();
+            var res = goalRep.GetTopGoalScorers("1", "2023/2024", "2023-08-12", "2024-05-19");
+            Console.WriteLine();
         }
     }
 }
