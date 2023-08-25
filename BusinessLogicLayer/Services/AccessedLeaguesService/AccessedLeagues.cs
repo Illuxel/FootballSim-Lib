e5d64bb8770a795550909b4f4247d6c92afca580
Daniil <ternak04@gmail.com>
2023-08-26 00:15:23 +0300

    AccessedLeagues service implementation

---
+++ BusinessLogicLayer/Services/AccessedLeaguesService/AccessedLeagues.cs
@@ -0,0 +1,67 @@
+﻿using DatabaseLayer;
+using DatabaseLayer.Enums;
+using DatabaseLayer.Repositories;
+using DatabaseLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLogicLayer.Services
+{
+    public class AccessedLeagues
+    {
+        TeamRepository _teamRepository;
+        LeagueRepository _leagueRepository;
+
+        private static int _LeagueRangeForSecondLevel = 2; 
+        public AccessedLeagues()
+        {
+            _teamRepository = new TeamRepository();
+            _leagueRepository = new LeagueRepository();
+        }
+
+        public List<League> DefineAccessedLeagues(PlayerGameData playerGameData)
+        {
+            var team = defineTeam(playerGameData.ClubId);
+            var accessedLeagues = defineLeagues(team,playerGameData);
+            return accessedLeagues;
+        }
+
+        private Team defineTeam(string teamId)
+        {
+            return _teamRepository.Retrieve(teamId);
+        }
+
+        private List<League> defineLeagues(Team team,PlayerGameData playerGameData)
+        {
+            var scoutLeague = team.League;
+            if(playerGameData.CurrentLevel == ScoutSkillLevel.Level1)
+            {
+                return new List<League>() { scoutLeague };
+            }
+            else if(playerGameData.CurrentLevel == ScoutSkillLevel.Level2)
+            {
+                var accessedLeagues = new List<League>();
+                var allLeagues = _leagueRepository.Retrieve();
+                var sortedLeagues = allLeagues.OrderBy(x => x.CurrentRating).ToList();  
+                
+                int maxIndex = sortedLeagues.Count - 1;
+                int scoutLeagueIndex = sortedLeagues.FindIndex(x => x.Id == scoutLeague.Id);
+                int lastLeftNeighbourIndex = scoutLeagueIndex - _LeagueRangeForSecondLevel;
+                int lastRightNeighbourIndex = scoutLeagueIndex + _LeagueRangeForSecondLevel;
+
+                for(int i = lastLeftNeighbourIndex;i <= lastRightNeighbourIndex;i++)
+                {
+                    if (i >= 0 && i <= maxIndex)
+                    {
+                        accessedLeagues.Add(sortedLeagues[i]);
+                    }
+                }
+                return accessedLeagues;
+
+            }
+            return _leagueRepository.Retrieve();
+        }
+    }
+}
--- StartupProject/Program.cs
+++ StartupProject/Program.cs
@@ -1,5 +1,7 @@
 using BusinessLogicLayer.Scenario;
 using BusinessLogicLayer.Services;
+using DatabaseLayer.Enums;
+using DatabaseLayer.Services;
 using System;
 
 namespace StartupProject
@@ -8,6 +10,35 @@ namespace StartupProject
     {
         public static void Main(string[] args)
         {
+            var data = new PlayerGameData()
+            {
+                CurrentLevel = ScoutSkillLevel.Level2,
+                Money = 1000000,
+                Role = UserRole.Scout,
+                PlayerName = "Test",
+                PlayerSurname = "Test",
+                RealDate = DateTime.Now.ToString("yyyy-MM-dd"),
+                GameDate = "2023-01-01",
+                ClubId = "656BD8A423624CD8D63235FCC8067E89",
+            };
+            var instance = LoadGameManager.GetInstance();
+            var save = instance.Load("1");
+            if (save == null)
+            {
+                instance.NewGame(data, "1");
+                save = instance.Load("1");
+            }
+            var playerData = save.PlayerData;
+
+
+            var service = new AccessedLeagues();
+            var team = service.DefineAccessedLeagues(playerData);
+            foreach (var item in team)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.CurrentRating);
+            }
         }
     }
 }
