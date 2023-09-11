6765ec8f1b2e00c8bbe0fe2c8006b1de8134883d
Daniil <ternak04@gmail.com>
2023-09-11 22:38:12 +0300

    Revert "refact"
    This reverts commit e50802819c7ba41b10d044e1074c79d5f47b61ac.

--- StartupProject/Program.cs
+++ StartupProject/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace StartupProject
+﻿using BusinessLogicLayer.Scenario;
+
+namespace StartupProject
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var gen = new GenerateGameActionsToNextMatch(new System.DateTime(2023,08,12), "678065FDDB06C590A0D0F9EDC2B5196F");
+            gen.SimulateActions();
         }
     }
 }
