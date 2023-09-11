ac0bb9c3d75270c037249603e3cdf7c02d987174
Daniil <ternak04@gmail.com>
2023-09-11 22:38:45 +0300

    refact

--- StartupProject/Program.cs
+++ StartupProject/Program.cs
@@ -1,13 +1,9 @@
-﻿using BusinessLogicLayer.Scenario;
-
-namespace StartupProject
+﻿namespace StartupProject
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var gen = new GenerateGameActionsToNextMatch(new System.DateTime(2023,08,12), "678065FDDB06C590A0D0F9EDC2B5196F");
-            gen.SimulateActions();
         }
     }
 }
